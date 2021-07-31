1a2281250375dc2662ea218dbcd1de08ccde7f36
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-07-31 00:10:10 -0400

    Changed an integer parser

--- app/controllers/invoice_numbers_controller.rb
+++ app/controllers/invoice_numbers_controller.rb
@@ -6,7 +6,7 @@ class InvoiceNumbersController < ApplicationController
 
         if @invoice.present?
             invoice_number = @invoice.invoice_number[2..-1]
-            next_number = Integer(invoice_number) + 1
+            next_number = invoice_number.to_i + 1
 
             next_invoice_number = year.to_s + next_number.to_s.rjust(2, "0")
         else
