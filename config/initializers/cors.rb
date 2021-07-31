35ff57a983437bcf0468f1a0b163554ca89e6136
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2021-07-31 09:05:43 -0400

    Added cors rules

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000', '*.jrexpress.netlify.app', 'jrexpress.netlify.app'
 
     resource '*',
       headers: :any,
