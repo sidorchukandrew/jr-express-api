98a11d8da6c75082c91fe911a5f2ca0a25f33003
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2022-01-05 00:05:48 -0500

    Changed scope to class method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,13 +3,15 @@ class Invoice < ApplicationRecord
     has_many_attached :attachments
     has_many :emails
 
-    scope :most_recent_this_year, -> {where("invoice_number LIKE ?", "#{Time.current.year % 100}%").order(invoice_number: :desc).first}
-
     def to_hash
         invoice_hash = self.as_json
         invoice_hash[:pdf_url] = self.pdf.url if self.pdf.attached?
         invoice_hash[:emails] = self.emails.as_json
         invoice_hash
+    end
+
+    def self.most_recent_this_year
+        where("invoice_number LIKE ?", "#{Time.current.year % 100}%").order(invoice_number: :desc).first
     end
 
 end
