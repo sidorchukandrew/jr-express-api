993532697027e011e870abe4831f771baa5723c0
Andrew Sidorchuk <sidorchukandrew@gmail.com>
2022-12-19 11:11:02 -0500

    Order by created date

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,7 +7,7 @@ class InvoicesController < ApplicationController
 
   # GET /invoices
   def index
-    @invoices = Invoice.with_attached_pdf.all.order(invoice_number: :desc )
+    @invoices = Invoice.with_attached_pdf.all.order(created_at: :desc )
 
     invoices_hash = @invoices.map { |invoice| invoice.to_hash }
 
